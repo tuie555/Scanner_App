c286bc22fab70bdf089a49de15cf222b0c6517f1
Ling4826 <143516833+Ling4826@users.noreply.github.com>
2025-01-21 20:40:35 +0700

    ok no bit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.test1"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.test1"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
