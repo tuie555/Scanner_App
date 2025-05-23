925e8569ae50c98bdef7b333fa54e97cf6eff9b2
Jord <90235483+tuie555@users.noreply.github.com>
2025-05-23 11:23:37 +0700

    remove sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,16 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.compose.compiler) apply false
-    
+    alias(libs.plugins.sonarqube)
 }
+sonar {
+    properties {
+        property("sonar.projectKey", "tuie555_Scanner_App")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property ("sonar.organization", "scannerapp")
+        property( "sonar.login", System.getenv("SONAR_TOKEN"))
+        property("sonar.sourceEncoding", "UTF-8")
+        property("sonar.androidLint.reportPaths", "app/build/reports/lint-results.xml")
 
+    }
+}
