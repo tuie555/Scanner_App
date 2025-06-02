660bc9035d45d47d6a9f93e692e8e3a2e847b640
Jord <90235483+tuie555@users.noreply.github.com>
2025-06-02 21:47:57 +0700

    แก้bug responsive

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.LingTH.fridge"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
+        versionCode = 15
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
